9ba815ad6308178a85deb05d30200920a00c4cd1
Zalkar <zalkar@bennington.edu>
2019-11-21 10:59:13 -0500

    trying different regex expressions

--- index.js
+++ index.js
@@ -13,4 +13,21 @@ var fs = require('fs');
 // Synchronous (blocking) way
 //
 var contents = fs.readFileSync('./test.md', 'utf8');
-console.log(contents);
+//console.log(contents);
+
+// function getValueByKey(text, key){
+//     var regex = new RegExp("images");
+//     var match = regex.exec(text);
+//     if(match)
+//         return match[1];
+//     else
+//         return null;
+// }
+
+// console.log(getValueByKey(contents, "images"));
+
+var paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+var regex = /!\images/g;
+var found = contents.match(regex);
+
+console.log(found);
