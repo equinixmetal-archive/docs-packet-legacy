9b8edabf1a088f2959ab75006e36d1c33c52aae7
Zalkar <zalkar@bennington.edu>
2019-11-25 22:51:40 -0500

    obtained path string and able to test if exists

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+
+const test = 'images/api-integrations/Project-ID.png';
+
+const exists = fs.existsSync(test);
+console.log(exists);
+
 
 var contents = fs.readFileSync('./test.md', 'utf8');
 
@@ -6,5 +13,8 @@ var regex = /(\!\[.+\]\(.+\))/g;
 var found = contents.match(regex);
 
 for (var i = 0; i < found.length; i++) {
-    console.log(found[i], typeof[found[i]]);
+    var temp_path = JSON.stringify(found[i], null, 2);
+    var firstIndex = temp_path.indexOf('/images');
+    var new_path = temp_path.substring(firstIndex + 1, temp_path.length - 2);
+    console.log(fs.existsSync(new_path));
 }
