7bb54256d95eb54b929107b1b8cb599efeed91cf
Zalkar <zalkar@bennington.edu>
2019-11-25 22:04:55 -0500

    cleaned up the script

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 var fs = require('fs');
 
-//
-// Asynchronous (non-blocking) way
-//
-// fs.readFile('./test.txt', 'utf8', function(err, contents) {
-//     console.log(contents);
-// });
-
-// console.log('this shows up before the contents of test.txt');
-
-//
-// Synchronous (blocking) way
-//
 var contents = fs.readFileSync('./test.md', 'utf8');
-//console.log(contents);
-
-// function getValueByKey(text, key){
-//     var regex = new RegExp("images");
-//     var match = regex.exec(text);
-//     if(match)
-//         return match[1];
-//     else
-//         return null;
-// }
-
-// console.log(getValueByKey(contents, "images"));
 
 var regex = /(\!\[.+\]\(.+\))/g;
 var found = contents.match(regex);
