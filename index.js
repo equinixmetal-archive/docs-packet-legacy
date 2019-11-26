e1b1e0157ac52dcba1c73ecd9c0fc6763596df1b
Zalkar <zalkar@bennington.edu>
2019-11-25 23:13:47 -0500

    initial testing complete, added edge cases'

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ const path = require('path');
 
 const test = 'images/api-integrations/Project-ID.png';
 
-const exists = fs.existsSync(test);
-console.log(exists);
+function walkDir(dir, callback) {
+    fs.readdirSync(dir).forEach( f => {
+      let dirPath = path.join(dir, f);
+      let isDirectory = fs.statSync(dirPath).isDirectory();
+      isDirectory ? 
+        walkDir(dirPath, callback) : callback(path.join(dir, f));
+    });
+  };
+
+walkDir('./products', function(filePath) {
+    if (path.extname(filePath) == '.md') {
+        
+
+        var contents = fs.readFileSync(filePath, 'utf8');
+
+        var regex = /(\!\[.+\]\(.+\))/g;
+        var found = contents.match(regex);
+
+        if (found) {
+            console.log("----------");
+            console.log(filePath);
+            console.log("----------");
+            for (var i = 0; i < found.length; i++) {
+                var temp_path = JSON.stringify(found[i], null, 2);
+                var firstIndex = temp_path.indexOf('/images');
+                var new_path = temp_path.substring(firstIndex + 1, temp_path.length - 2);
+                var exists = fs.existsSync(new_path);
+                if (!exists) console.log("Missing image: ", new_path);
+            }
+        }
+    }
+});
 
 
-var contents = fs.readFileSync('./test.md', 'utf8');
-
-var regex = /(\!\[.+\]\(.+\))/g;
-var found = contents.match(regex);
-
-for (var i = 0; i < found.length; i++) {
-    var temp_path = JSON.stringify(found[i], null, 2);
-    var firstIndex = temp_path.indexOf('/images');
-    var new_path = temp_path.substring(firstIndex + 1, temp_path.length - 2);
-    console.log(fs.existsSync(new_path));
-}
+//Missing image:  "![Screen-Shot-2018-12-03-at-8.31.48-PM.png](https://support.packet.com/file.php/local/303905WCCXWAGDARANNMD0/Screen-Shot-2018-12-03-at-8.31.48-PM.png
+//![add VLAN jpg](https://raw.githubusercontent.com/packethost/docs/master/images/layer-2-overview/add-vlan.jpg "Add a VLAN")
